22483a97a89cb9cdac8e49d7c7c768f1ac09c275
niyambhat <niyambhat@outlook.com>
2023-05-28 15:28:59 +0800

    Succesfully integrated Category creation, and redirection

--- BulkyWeb/Models/Category.cs
+++ BulkyWeb/Models/Category.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 namespace BulkyWeb.Models
 {
 	public class Category
@@ -6,7 +7,10 @@ namespace BulkyWeb.Models
 		[Key]
 		public int Id { get; set; }
 		[Required]
-		public string Name { get; set; }
+		[MaxLength(30)]
+        [DisplayName("Category Name")]
+        public string Name { get; set; }
+		[Range(1,100)]
 		public int DisplayOrder { get; set; }
 	}
 }
