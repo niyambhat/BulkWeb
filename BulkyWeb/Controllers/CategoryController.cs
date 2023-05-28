00e4544b445a3b406eb5df66e602e33a421944df
niyambhat <niyambhat@outlook.com>
2023-05-28 10:47:53 +0800

    Accesing db

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -3,19 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using BulkyWeb.Data;
+using BulkyWeb.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BulkyWeb.Controllers
 {
+
     public class CategoryController : Controller
     {
         //accesing the db for operations
         private readonly ApplicationDbContext _db;
-        public CategoryController(ApplicationDbContext db) {
+        public CategoryController(ApplicationDbContext db)
+        {
             _db = db;
         }
         public IActionResult Index()
         {
+            List<Category> objCategorylist = _db.Categories.ToList();
             return View();
         }
     }
